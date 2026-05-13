1042a87464efc157139be2f4fa2ea17a7c44d8ca
lfvbdghkjfgm <topychkanov123@yandex.ru>
2026-05-13 19:44:48 +0300

    speedtest update

--- prikol/speedtest.py
+++ prikol/speedtest.py
@@ -14,12 +14,9 @@ def click_button(xpath: str):
     button.click()
 
 
-k = 0
-for x in open("creds.txt"):
-    login, password = x.split()
-
+def login(login: str, password: str):
     driver.get("https://examinf.ru/")
-
+    time.sleep(0.5)
     click_button("//button[text()='Войти']")
 
     login_field = driver.find_element(
@@ -28,48 +25,71 @@ for x in open("creds.txt"):
     password_field = driver.find_element(
         By.XPATH, '//*[@id="authOpenButtons"]/div[1]/div/form/input[2]'
     )
-
     login_field.send_keys(login)
     password_field.send_keys(password)
-
+    time.sleep(0.5)
     click_button('//*[@id="authOpenButtons"]/div[1]/div/form/input[3]')
-    # click_button('//*[@id="ROOT"]/div/header/div/a[3]')
-    # click_button('//*[@id="ROOT"]/div/div[2]/div[2]/div/div[3]/a')
-    # click_button('//*[@id="ROOT"]/div/div[2]/div/div/button')
-    #
-    # lines = wait.until(
-    #     lambda d: d.find_elements(
-    #         By.CSS_SELECTOR, ".CodeMirror-code pre.CodeMirror-line"
-    #     )
-    # )
 
-    # code = "\n".join(line.text for line in lines).strip()
-    # # tm = len(code) * 60 / 725
-    # tm = len(code) * 60 / 600
-    # time.sleep(tm)
 
-    # driver.execute_script(
-    #     """
-    #     const editor = document.querySelector('#examinf-typing-cm-mount .CodeMirror').CodeMirror;
-    #     editor.setValue(arguments[0]);
-    #     editor.focus();
-    # """,
-    #     code,
-    # )
-    # input()
+def speedtest():
+    driver.get("https://examinf.ru/")
+    click_button('//*[@id="ROOT"]/div/header/div/a[3]')
+    click_button('//*[@id="ROOT"]/div/div[2]/div[2]/div/div[3]/a')
+    click_button('//*[@id="ROOT"]/div/div[2]/div/div/button')
 
-    driver.get("https://examinf.ru/account/")
-    time.sleep(2)
-    name = driver.find_element(
-        By.XPATH, '//*[@id="ROOT"]/div/div[2]/div[2]/div/div[1]/div/input[1]'
+    lines = wait.until(
+        lambda d: d.find_elements(
+            By.CSS_SELECTOR, ".CodeMirror-code pre.CodeMirror-line"
+        )
     )
-    surname = driver.find_element(
-        By.XPATH, '//*[@id="ROOT"]/div/div[2]/div[2]/div/div[1]/div/input[2]'
+
+    code = "\n".join(line.text for line in lines).strip()
+    # tm = len(code) * 60 / 725
+    tm = len(code) * 60 / 600
+    time.sleep(tm)
+
+    driver.execute_script(
+        """
+        const editor = document.querySelector('#examinf-typing-cm-mount .CodeMirror').CodeMirror;
+        editor.setValue(arguments[0]);
+        editor.focus();
+    """,
+        code,
+    )
+    input()
+
+
+def set_data(name_inp, surname_inp):
+    driver.get("https://examinf.ru/account/")
+    name = wait.until(
+        Ec.element_to_be_clickable(
+            (By.XPATH, '//*[@id="ROOT"]/div/div[2]/div[2]/div/div[1]/div/input[1]')
+        )
+    )
+    surname = wait.until(
+        Ec.element_to_be_clickable(
+            (By.XPATH, '//*[@id="ROOT"]/div/div[2]/div[2]/div/div[1]/div/input[2]')
+        )
     )
     surname.clear()
     name.clear()
-    surname.send_keys("pwned")
-    name.send_keys("by lfvbdghkjfgm")
+    surname.send_keys(surname_inp)
+    name.send_keys(name_inp)
+    time.sleep(0.5)
+
+
+k = 0
+for x in open("creds.txt"):
+    log, password = x.split()
+
+    driver.get("https://examinf.ru/")
+
+    login(log, password)
+
+    # speedtest()
+
+    set_data("by lfvbdghkjfgm", "pwned")
+
     click_button('//*[@id="ROOT"]/div/div[2]/div[1]/button')
     k += 1
     if k == 5:
